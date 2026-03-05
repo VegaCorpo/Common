7b68cf1944304166ccc98c572bd350fd1b2dc7a0
baptistebd <baptiste.blambert@epitech.eu>
2026-03-05 16:23:16 +0100

    feat: interfaces folder with ILayer and IUIEngine

---
+++ include/interfaces/ILayer.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "types/RenderBuffer.hpp"
+
+namespace common {
+    class ILayer {
+        public:
+            virtual ~ILayer() = default;
+
+            // Called each frame to update the UI
+            virtual void beginFrame(float deltaTime, float width, float height) = 0;
+
+            // Returns the universal render buffer for the current frame
+            virtual RenderDataBuffer& getFrame() = 0;
+
+            // Shutdown / cleanup
+            virtual void shutdown() = 0;
+    };
+} // namespace ui
