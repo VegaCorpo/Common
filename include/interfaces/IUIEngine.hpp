8076c5f14ca985f7be20e59cd852c1b05acc7f28
baptistebd <baptiste.blambert@epitech.eu>
2026-03-10 10:18:24 +0100

    feat: change init methods to allow Texture Loader

--- include/interfaces/IUIEngine.hpp
+++ include/interfaces/IUIEngine.hpp
@@ -1,16 +1,20 @@
 #pragma once
 
+#include <functional>
+
 #include "../types/RenderDataBuffer.hpp"
 
 namespace common {
     class IUIEngine {
+
+        using TextureLoader = std::function<unsigned int(unsigned char*, int, int)>;
 
         public:
             // Destructor
             virtual ~IUIEngine() = default;
 
             // Init methods that must be call by Core
-            virtual void init() = 0;
+            virtual void init(TextureLoader loader) = 0;
 
             // Update UI frame and convert it for the renderer
             virtual void update(float dt, float w, float h) = 0;
