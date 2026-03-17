ee052edfe7dac19be1a5c7718590b1c6b7f27593
Axel Lavrador <80172526+Oursegamin@users.noreply.github.com>
2026-03-17 13:43:41 +0100

    Texture and size component (#16)
    * feat: textureIds and radius
    
    * fix: textureId type
    
    * feat: default texture id
    
    * fix: namespace
    
    * feat: cpp 23
    
    * chore: remove include directory

---
+++ utils/textureId.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+namespace common {
+    std::string ASSETS_PATH = "Renderer/assets/";
+    std::string TEXTURE_PATH = ASSETS_PATH + "textures/";
+
+    constexpr char DEFAULT_TEXTURE_ID[] = "DEFAULT";
+
+    const std::unordered_map<std::string, std::string> textureMap = {
+        {"Sun", TEXTURE_PATH + "sun.jpg"},
+        {"Earth", TEXTURE_PATH + "earth.jpg"},
+        {"Moon", TEXTURE_PATH + "moon.jpg"}
+    };
+} // namespace common
