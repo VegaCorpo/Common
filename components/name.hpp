a48c74f28de301271e03df6b8b891b7e730eead6
Axel Lavrador <80172526+Oursegamin@users.noreply.github.com>
2026-03-17 13:59:34 +0100

    fix: constant conflicts (#18)

--- components/name.hpp
+++ components/name.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 namespace common::components {
-    static constexpr unsigned char MAX_SIZE = 50;
+    static constexpr unsigned char MAX_NAME_LENGTH = 50;
     struct Name {
-            char value[MAX_SIZE];
+            char value[MAX_NAME_LENGTH];
     };
 } // namespace common::components
