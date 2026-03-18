9a8b8b931e2ca161625adc3d9b651aa40a8081db
baptistebd <baptiste.blambert@epitech.eu>
2026-03-18 15:19:05 +0100

    fix: add window in init parameter

--- include/interfaces/IUIEngine.hpp
+++ include/interfaces/IUIEngine.hpp
@@ -3,6 +3,7 @@
 #include <functional>
 
 #include "../types/RenderDataBuffer.hpp"
+#include <GLFW/glfw3.h>
 
 namespace common {
 
@@ -15,7 +16,7 @@ namespace common {
             virtual ~IUIEngine() = default;
 
             // Init methods that must be call by Core
-            virtual void init() = 0;
+            virtual void init(GLFWwindow* window) = 0;
 
             // Update UI frame and convert it for the renderer
             virtual void update(float dt, float w, float h) = 0;
