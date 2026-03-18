12049f2cdf119ed1d176969bab1e4fc3ab7c56d6
Oursegamin <axel.lavrador@gmail.com>
2026-03-18 14:24:02 +0100

    fix: missing texture name

--- utils/assets.hpp
+++ utils/assets.hpp
@@ -7,5 +7,5 @@ namespace common {
     inline const std::string ASSETS_PATH = "Renderer/assets/";
     inline const std::string TEXTURE_PATH = ASSETS_PATH + "textures/";
 
-    constexpr char DEFAULT_TEXTURE_PATH[components::MAX_TEXTURE_PATH_LENGTH] = "default.jpg";
+    constexpr char DEFAULT_TEXTURE_PATH[components::MAX_TEXTURE_PATH_LENGTH] = "missing_texture.jpg";
 } // namespace common
