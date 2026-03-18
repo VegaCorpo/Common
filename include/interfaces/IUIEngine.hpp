3822d17b00f568b855ac0b6f39ea75b07e58ed3d
baptistebd <baptiste.blambert@epitech.eu>
2026-03-18 11:56:33 +0100

    del: loader parameter

--- include/interfaces/IUIEngine.hpp
+++ include/interfaces/IUIEngine.hpp
@@ -15,7 +15,7 @@ namespace common {
             virtual ~IUIEngine() = default;
 
             // Init methods that must be call by Core
-            virtual void init(TextureLoader loader) = 0;
+            virtual void init() = 0;
 
             // Update UI frame and convert it for the renderer
             virtual void update(float dt, float w, float h) = 0;
