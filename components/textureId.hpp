53813012b693787e6877bed61b25e5988d388c6d
Axel Lavrador <80172526+Oursegamin@users.noreply.github.com>
2026-03-18 14:56:57 +0100

    Texture and render interface rework (#21)
    * feat: add syncIn to the RedererEngine
    
    * fix: remove texture ids to path (put the relative texture path in scene configs)
    
    * fix: missing texture name

--- components/textureId.hpp
+++ components/textureId.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 namespace common::components {
-    static constexpr unsigned char MAX_TEXTURE_ID_LENGTH = 50;
-    struct TextureId {
-            char value[MAX_TEXTURE_ID_LENGTH];
+    static constexpr unsigned char MAX_TEXTURE_PATH_LENGTH = 50;
+    struct Texture {
+            char path[MAX_TEXTURE_PATH_LENGTH];
     };
 } // namespace common::components
--- utils/assets.hpp
+++ utils/assets.hpp
@@ -2,17 +2,10 @@
 
 #include "../components/textureId.hpp"
 #include <string>
-#include <unordered_map>
 
 namespace common {
     inline const std::string ASSETS_PATH = "Renderer/assets/";
     inline const std::string TEXTURE_PATH = ASSETS_PATH + "textures/";
 
-    constexpr char DEFAULT_TEXTURE_ID[components::MAX_TEXTURE_ID_LENGTH] = "Default";
-
-    inline const std::unordered_map<std::string, std::string> textureMap = {
-        {"Sun", TEXTURE_PATH + "sun.jpg"},
-        {"Earth", TEXTURE_PATH + "earth.jpg"},
-        {"Moon", TEXTURE_PATH + "moon.jpg"}
-    };
+    constexpr char DEFAULT_TEXTURE_PATH[components::MAX_TEXTURE_PATH_LENGTH] = "missing_texture.jpg";
 } // namespace common
