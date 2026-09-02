9e98a0addd70e17909181dafaa3a4f284931b7ee
TilioHuart <tilio.huart@epiteh.eu>
2026-09-02 09:08:05 +0900

    feat: update data structure

--- types/World.hpp
+++ types/World.hpp
@@ -5,13 +5,33 @@
 #include "../components/velocity.hpp"
 #include "../components/acceleration.hpp"
 #include "../components/mass.hpp"
+#include "../components/name.hpp"
+#include "../components/radius.hpp"
+#include "../components/texture.hpp"
 
 namespace common {
     struct WorldState {
-        std::vector<size_t> entities;
+        std::vector<size_t> entitiesId;
         std::vector<common::components::Position> positions;
         std::vector<common::components::Velocity> velocities;
         std::vector<common::components::Acceleration> accelerations;
-        std::vector<common::components::Mass> mass;
+    };
+
+    struct SpecificDataPhysics {
+        std::vector<size_t> entitisId;
+        std::vector<common::components::Mass> masses;
+    };
+
+    struct SpecificDataRender {
+        std::vector<size_t> entitisId;
+        std::vector<common::components::Name> names;
+        std::vector<common::components::Radius> radius;
+        std::vector<common::components::Texture> textures;
+    };
+
+    struct SpecificDataUI {
+        std::vector<size_t> entitisId;
+        std::vector<common::components::Name> names;
+        std::vector<common::components::Mass> masses;
     };
 }
